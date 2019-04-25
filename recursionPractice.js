d790f49b6350adf6f066356c726894bc068969fa
Tim Mickle <TimothyRMickle@gmail.com>
2019-04-25 18:04:42 -0500

    remove Duplicate letter recursively chall

--- recursionPractice.js
+++ recursionPractice.js
@@ -38,11 +38,6 @@ function reverseStr(str) {
 // In the example of recursion we may think of the answer to a question as leading to another question.  What is the fib of 4? Well it is the fib of 2 + fib of 3, well what is the fib of 2 or fib of 3? Well the fib of 2 is fib of 1 + fib of 0 ,(etc) 
 
 
-
-
-
-
-
 function fibonacci (position) {
     if (position <= 0) {
         return 0 
@@ -54,4 +49,60 @@ function fibonacci (position) {
     return fibonacci(position-1) + fibonacci(position-2)
 } 
 
-console.log ( fibonacci(15) )
+// console.log ( fibonacci(15) )
+
+//-----------------------------------------------------------------------//
+
+
+//Using recursion, go through a string and remove characters that occur more than once. E.g. passing in "Troll" should return "trol". Passing in "abracadabra" should return "abrcd".
+
+// removeDupLetter ('Troll') = > trol
+// removeDupLetter ('abracadabra') = > abrcd
+
+function removeDupLetter (str) {
+
+    if (str.length <= 0) {
+        return ''
+    } 
+
+    let firstLetter = str[0]
+
+    let regex = new RegExp(firstLetter, 'gi')
+
+    str = str.replace(regex,'')
+
+
+    return firstLetter + removeDupLetter(str)
+    
+}
+
+let testStr = 'ttttrrrrroooolll'
+console.log( removeDupLetter(testStr) )
+console.log(testStr)
+
+
+/////////////// ARTICLE'S SOLUTION //////////
+function removeReoccurringCharacters(str) {
+    if (typeof str !== "string") {
+        throw new Error("Please enter a string!");
+    }
+    var charMap = {};
+    var result = [];
+    var removeRepeatingChar = function(str) {
+        var len = str.length;
+        // base case
+        if (len === 0) {
+            return str;
+        }
+        var firstChar = str.charAt(0);
+        if (!charMap.hasOwnProperty(firstChar)) {
+            charMap[firstChar] = true;  // mark as having occurred
+            result.push(firstChar);     // push into results
+        }
+        removeRepeatingChar(str.substring(1, len));
+    };
+    removeRepeatingChar(str);
+    return result.join('');
+}
+
+console.log(removeReoccurringCharacters("creeeedisgiiiid")); 
